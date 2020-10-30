6a179456203fd18bf7f90ccb4d7fd3cf26c9fbb0
Serenity <d.palbanese@gmail.com>
2020-10-30 17:27:04 +0100

    Add data records for chat

---
+++ LirikWatchApi/LirikWatch.Common/Records/ChatRecords/Chat.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using LirikWatch.Common.Records.VideoRecords;
+
+namespace LirikWatch.Common.Records.ChatRecords
+{
+    public class Chat
+    {
+        public Video Video { get; init; }
+        
+        public List<Comment> Comments { get; init; }
+    }
+}
---
+++ LirikWatchApi/LirikWatch.Common/Records/VideoRecords/Video.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LirikWatch.Common.Records.VideoRecords
+{
+    public class Video
+    {
+        public string Title { get; init; }
+        
+        public ulong BroadcastId { get; init; }
+
+        public string Url { get; init; }
+
+        public DateTime CreatedAt { get; init; }
+
+        public string Id { get; init; }
+
+        public int LengthInSeconds { get; init; }
+    }
+}
