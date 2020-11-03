868bcdb6b492111c8aead1222524f947b3f0aebd
Serenity <d.palbanese@gmail.com>
2020-11-03 21:27:34 +0100

    Remove comment

--- LirikWatchApi/LirikWatch.WebApi/Startup.cs
+++ LirikWatchApi/LirikWatch.WebApi/Startup.cs
@@ -42,33 +42,6 @@ namespace LirikWatch.WebApi
                     Description = "API for LirikW Website"
                 });
                 
-                // c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-                // {
-                //     Description = "Used for JWT token",
-                //     Name = "Authorization",
-                //     In = ParameterLocation.Header,
-                //     Type = SecuritySchemeType.ApiKey,
-                //     Scheme = "Bearer"
-                // });
-                //
-                // c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                // {
-                //     {
-                //         new OpenApiSecurityScheme()
-                //         {
-                //             Reference = new OpenApiReference()
-                //             {
-                //                 Type = ReferenceType.SecurityScheme,
-                //                 Id = "Bearer"
-                //             },
-                //             Scheme = "oauth2",
-                //             Name = "Bearer",
-                //             In = ParameterLocation.Header
-                //         }, 
-                //         new List<string>()
-                //     }
-                // });
-                
                 // Set the comments path for the Swagger JSON and UI.
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
