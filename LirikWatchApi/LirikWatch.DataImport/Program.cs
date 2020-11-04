4a98ebc1801df49b82009da9dfc6a2c2c2d02d2d
Daniele Albanese <daniele.albanese@incubegroup.com>
2020-11-04 14:16:54 +0100

    Add data importer stump

---
+++ LirikWatchApi/LirikWatch.DataImport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LirikWatch.DataImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
